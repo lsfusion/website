683cc2c1cf91a421a4f635ca94ee5e7d9aad2f03
aramanenka <sasha.romanenko@gmail.com>
2021-09-20 21:54:57 +0300

    First commit

---
+++ index.php
@@ -0,0 +1,126 @@
+<?php
+    include "include/header.php";
+?>
+
+<header id="homeheader">
+    <div class="inner">
+        <h1>
+            Probably the <em class="sp">most declarative</em><br />
+            business application<br />
+            development platform ever
+        </h1>
+        <p>Develop much faster and simpler. Significantly increase your solutions perfomance, modularity and integrity. Up to x10 than ever before for complex solutions. And it's open source.</p>
+        <nav>
+            <a href="#">Try Online</a>
+            <a href="#">Demo App</a>
+        </nav>
+    </div>
+</header>
+
+<div class="inner">
+    <ul class="home-links1">
+        <li>
+            <div class="inner">
+                <h3>Blog</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam, purus sit amet luctus venenatis, lectus magna.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam, purus sit amet luctus venenatis, lectus magna.</p>
+            </div>
+        </li>
+        <li>
+            <div class="inner">
+                <h3>How to use?</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam, purus sit amet luctus venenatis, lectus magna.</p>
+            </div>
+        </li>
+    </ul>
+    <h2>Based on the <em class="sp">most advanced</em> programming paradigms</h2>
+    <ul class="home-links2">
+        <li>
+            <div class="inner">
+                <h4>Function-level</h4>
+                <p>Pure functions with no side effects. Limited, however vast, number of operators (high order functions)</p>
+            </div>
+        </li>
+        <li>
+            <div class="inner">
+                <h4>Reactive</h4>
+                <p>Data and its views are automatically incrementally updated whenever any data they use changes</p>
+            </div>
+        </li>
+        <li>
+            <div class="inner">
+                <h4>Event-driven</h4>
+                <p>Control flow is split into a number of small events, whose execution order is automatically built by the platform. Vast capabilities of getting what exactly was changed are also essential.</p>
+            </div>
+        </li>
+        <li>
+            <div class="inner">
+                <h4>Object-oriented</h4>
+                <p>Support of inheritance and polymorphism, including multiple ones for both of them. Encapsulation is also encouraged, however not necessary.</p>
+            </div>
+        </li>
+        <li>
+            <div class="inner">
+                <h4>Constraint</h4>
+                <p>Programming with declarative rules rather than imperative checks. This rules cannot be broken and often are used to automatically build proper UI. </p>
+            </div>
+        </li>
+        <li>
+            <div class="inner">
+                <h4>Meta</h4>
+                <p>Code templating and autogeneration. Both in IDE and in run-time.</p>
+            </div>
+        </li>
+    </ul>
+
+    TODO:
+
+    <h2>Find out more about lsFusion</h2>
+    <ul class="home-links4">
+        <li>
+            <div class="inner">
+                <h3>Features</h3>
+                <p>Lipsum dolor sit amet, consectetur adipiscing elit. Augue felis tincidunt phasellus nulla semper a tristique.</p>
+            </div>
+        </li>
+        <li>
+            <div class="inner">
+                <h3>Compare</h3>
+                <p>Lipsum dolor sit amet, consectetur adipiscing elit. Augue felis tincidunt phasellus nulla semper a tristique.</p>
+            </div>
+        </li>
+    </ul>
+    <div id="home-footer">
+        <h3>Solutions</h3>
+        <ul>
+            <li>
+                <div class="inner">
+                    <h4>MyCompany</h4>
+                    <p>Open-source solution for small businesses</p>
+                </div>
+            </li>
+            <li>
+                <div class="inner">
+                    <h4>lsFusion ERP</h4>
+                    <p>ERP-solution for large retail and wholesale companies</p>
+                </div>
+            </li>
+            <li>
+                <div class="inner">
+                    <h4>lsFusion WMS</h4>
+                    <p>Elit ut aliquam, purus sit amet luctus venenatis</p>
+                </div>
+            </li>
+            <li>
+                <div class="inner">
+                    <h4>More solutions</h4>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing.</p>
+                </div>
+            </li>
+        </ul>
+    </div>
+</div>
+
+<?php
+    include "include/footer.php";
+?>
---
+++ js/app.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+
+    $(document).click(function(e){
+
+        let target = e.target;
+
+        if( $(target).is(".language") || $(target).closest(".language").size() > 0 ){
+            $("div.language").toggleClass("active");
+        }else{
+            $("div.language").removeClass("active");
+        }
+    })
+    $(".menu-link").click(function(){$(this).toggleClass("active")})
+})
